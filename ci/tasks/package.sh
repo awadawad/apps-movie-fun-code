e417bfdd54a48377eb28b50ea521275cc0e3cc88
Awad Awad <email@awadawad.com>
2017-10-12 16:45:13 -0600

    Reverted to war packaging

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -3,18 +3,18 @@
 set -e +x
 
 pushd movie-fun-source
-  echo "Packaging JAR"
+  echo "Packaging WAR"
   ./mvnw clean package -DskipTests
 popd
 
-jar_count=`find movie-fun-source/target -type f -name *.jar | wc -l`
+war_count=`find movie-fun-source/target -type f -name *.war | wc -l`
 
-if [ $jar_count -gt 1 ]; then
-  echo "More than one jar found, don't know which one to deploy. Exiting"
+if [ $war_count -gt 1 ]; then
+  echo "More than one war found, don't know which one to deploy. Exiting"
   exit 1
 fi
 
-find movie-fun-source/target -type f -name *.jar -exec cp "{}" package-output/movie-fun.jar \;
+find movie-fun-source/target -type f -name *.war -exec cp "{}" package-output/movie-fun.war \;
 
 echo "Done packaging"
 exit 0
